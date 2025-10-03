135e5c8407f35bf88afaaacaae1ac8455ae1d33b
Chetan Bisht <chetanbisht6113@gmail.com>
2025-10-03 19:00:25 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = 3000;
+
+const corsOptions = {
+    origin: '*'
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
